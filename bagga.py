a251cb26b9003036d017ab4410914e9aba338b02
ab5y <ab5s10@gmail.com>
2016-03-11 20:08:00 +0530

    hello world

---
+++ bagga.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, session, g, redirect, url_for, \
+				abort, render_template, flash
+
+# Configuation
+USERNAME = 'admin'
+PASSWORD = 'default'
+# DEBUG = True
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route('/')
+def hello():
+	print 'Reaches here'
+	return 'Hello world' # render_template('show_landing.html')
+
+if __name__ == '__main__':
+	app.run()
